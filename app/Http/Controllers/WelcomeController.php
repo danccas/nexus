910627c2c7011a247eaf6c415314a5d1eb8065ab
Diego Anccas <diego.anccas@gmail.com>
2023-06-06 13:20:24 -0500

    path

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Core\Controller;
 
-class LoginController extends Controller
+class WelcomeController extends Controller
 {
     function index()
     {
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,4 @@
 
 use Core\Route;
 
-Route::get('', 'App\Http\Controllers\DashboardController@redirect')->middleware('auth');
-Route::get('dashboard', 'App\Http\Controllers\DashboardController@index')->name('dashboard.index')->middleware('auth');
-Route::get('dashboard/api/onpremise_top', 'App\Http\Controllers\DashboardController@api_onpremise_top');
-Route::get('dashboard/api/onpremise', 'App\Http\Controllers\DashboardController@api_onpremise');
+Route::get('', 'App\Http\Controllers\WelcomeController@index');
