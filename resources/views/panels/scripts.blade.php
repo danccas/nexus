c96b7075fa8df0367f5715d61fa02b72b89181b2
Diego Anccas <diego.anccas@gmail.com>
2023-09-14 23:58:23 -0500

    Update scripts.blade.php

--- resources/views/panels/scripts.blade.php
+++ resources/views/panels/scripts.blade.php
@@ -1,3 +1,3 @@
-<script type="text/javascript" src="{{ asset('libs/require/require.js') }}"></script>
+<script type="text/javascript" src="{{ asset('libs/require/require.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('libs/jquery/jquery.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('libs/popup/popup.min.js') }}"></script>
