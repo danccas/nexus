3caf7a86cc127c41d4e99906b4d90e3f4bfef3a7
Diego Anccas <diego.anccas@gmail.com>
2023-09-19 22:01:44 -0500

    m

--- resources/views/panels/scripts.blade.php
+++ resources/views/panels/scripts.blade.php
@@ -1,4 +1,4 @@
-<script type="text/javascript" src="{{ asset('libs/require/require.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('libs/jquery/jquery.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('libs/popup/popup.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/libs/require/require.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/libs/jquery/jquery.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/libs/popup/popup.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/js/app.js') }}"></script>
