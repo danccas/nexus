7c42fa52e7ca8ae6c1f8df80e435fc4695353ecc
Diego Anccas <diego.anccas@gmail.com>
2023-09-22 16:31:35 -0500

    m

--- resources/views/panels/scripts.blade.php
+++ resources/views/panels/scripts.blade.php
@@ -1,3 +1,2 @@
-<script type="text/javascript" src="{{ asset('assets/libs/jquery/jquery.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('assets/libs/popup/popup.min.js') }}"></script>
+@NexusScripts
 <script type="text/javascript" src="{{ asset('assets/js/app.js') }}"></script>
